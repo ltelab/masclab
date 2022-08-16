a3add60306aa29bfb97b55b8523018afc54a7573
Jacopo Grazioli <jacopo.grazioli@epfl.ch>
2022-08-16 09:37:12 +0200

    minor modif

--- prediction/write_in_textfile_predictions_for_campaign.m
+++ prediction/write_in_textfile_predictions_for_campaign.m
@@ -2,9 +2,9 @@
 clear; close all;
 
 % MASC matlab structure to load
-struct_name = './data.mat';
+struct_name = '/ltedata/ICEGENESIS_2021/MASC/Proc_data/data_classification_20210115_20210131.mat';
 % saving results in ASCII file
-filename = './PLATO_2019_MASC_classification_full.dat';
+filename = '/ltedata/ICEGENESIS_2021/MASC/Proc_data/ICEGENESIS_2021_MASC_classification_20210115_20210131.dat';
 % resolution of MASC cameras [mm/pix]
 res = 0.033;
 
@@ -18,7 +18,7 @@ fid = fopen(filename,'w');
 
 % header
 fprintf(fid,'# particle-by-particle MASC classification output \n');
-fprintf(fid,'# columns header: 1) particle timestamp [UTC], 2) particle ID, 3) camera ID 4) particle type 5) particle degree of riming 6) is melting? 7) quality parameter 8) Dmax [mm] 9) Area [mm^2] 10) Aspect Ratio 11) Shape complexity 12) Orientation 13) Fallspeed 14) Mean brightness [0-1] 15) Maximum brightness [0-1] 16-21) Probabilities for hydrometeor classification \n');
+fprintf(fid,'# columns header: 1) particle_timestamp_[UTC], 2) particle_ID, 3) camera_ID, 4) particle_type, 5) particle_degree_of_riming, 6) is_melting?, 7) quality_parameter, 8) Dmax_[mm], 9) Area_[mm^2], 10) Aspect_Ratio, 11) Shape_complexity, 12) Orientation, 13) Fallspeed, 14) Mean_brightness_[0-1], 15) Maximum_brightness_[0-1], 16-21) Probabilities_for_hydrometeor_classification \n');
 fprintf(fid,'# columns delimiter = tabulation \n');
 fprintf(fid,'# particle types : 1 = small particle (SP), 2 = columnar crystal (CC), 3 = planar crystal (PC), 4 = aggregate (AG), 5 = graupel (GR), 6 = combination of columnar and planar crystals (CPC) \n');
 
